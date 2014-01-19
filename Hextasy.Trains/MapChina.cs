0fadf01c4c476b6d3c234fe37334a0787fef27d9
Mad <devdumbblind@googlemail.com>
2014-01-19 14:46:28 +0800

    increased size of China
    added USA map

--- Hextasy.Trains/MapChina.cs
+++ Hextasy.Trains/MapChina.cs
@@ -9,7 +9,7 @@ namespace Hextasy.Trains
         private readonly IEnumerable<string> _stations = new[]
         {
             "Urumqi", "Harbin", "Beijing", "Hotan", "Golmud", "Xian",
-            "Shanghai", "Lhasa", "Chengdu", "Guiyang", "Guangzhou"
+            "Shanghai", "Lhasa", "Chengdu", "Wuhan", "Kunming", "Guangzhou"
         };
 
         public string Name { get { return "China"; } }
@@ -20,70 +20,80 @@ namespace Hextasy.Trains
             switch (row)
             {
                 case 0:
-                    if (column == 12 || column == 13) return TrainsTile.Empty;
+                    if (column >= 15 && column <= 17) return TrainsTile.Empty;
                     break;
                 case 1:
-                    if (column == 3 || (column >= 12 && column <= 15)) return TrainsTile.Empty;
+                    if (column == 5 || (column >= 15 && column <= 19)) return TrainsTile.Empty;
                     break;
                 case 2:
-                    if ((column >= 3 && column <= 5) || (column >= 11 && column <= 15))
+                    if ((column >= 3 && column <= 5) || (column >= 15 && column <= 18))
                     {
-                        if (column == 3) return new StationTile("Urumqi");
-                        if (column == 14) return new StationTile("Harbin");
+                        if (column == 17) return new StationTile("Harbin");
                         return TrainsTile.Empty;
                     }
                     break;
                 case 3:
-                    if ((column >= 1 && column <= 5) || (column >= 10 && column <= 14)) return TrainsTile.Empty;
-                    break;
-                case 4:
-                    if ((column >= 0 && column <= 7) || (column >= 9 && column <= 12))
+                    if ((column >= 3 && column <= 7) || (column >= 13 && column <= 18))
                     {
-                        if (column == 1) return new StationTile("Hotan");
-                        if (column == 11) return new StationTile("Beijing");
+                        if (column == 5) return new StationTile("Urumqi");
                         return TrainsTile.Empty;
                     }
                     break;
+                case 4:
+                    if ((column >= 0 && column <= 9) || (column >= 11 && column <= 17)) return TrainsTile.Empty;
+                    break;
                 case 5:
-                    if (column >= 0 && column <= 11)
+                    if (column >= 0 && column <= 15)
                     {
-                        if (column == 5) return new StationTile("Golmud");
+                        if (column == 2) return new StationTile("Hotan");
+                        if (column == 14) return new StationTile("Beijing");
                         return TrainsTile.Empty;
                     }
                     break;
                 case 6:
-                    if (column >= 1 && column <= 12)
+                    if (column >= 1 && column <= 16)
                     {
-                        if (column == 9) return new StationTile("Xian");
+                        if (column == 7) return new StationTile("Golmud");
                         return TrainsTile.Empty;
                     }
                     break;
                 case 7:
-                    if (column >= 1 && column <= 13)
+                    if (column >= 1 && column <= 15)
                     {
-                        if (column == 13) return new StationTile("Shanghai");
+                        if (column == 12) return new StationTile("Xian");
                         return TrainsTile.Empty;
                     }
                     break;
                 case 8:
-                    if ((column >= 2 && column <= 4) || (column >= 6 && column <= 13))
+                    if (column >= 2 && column <= 17)
                     {
-                        if (column == 4) return new StationTile("Lhasa");
-                        if (column == 8) return new StationTile("Chengdu");
+                        if (column == 17) return new StationTile("Shanghai");
                         return TrainsTile.Empty;
                     }
                     break;
                 case 9:
-                    if (column >= 6 && column <= 13)
+                    if (column >= 4 && column <= 17)
                     {
-                        if (column == 9) return new StationTile("Guiyang");
+                        if (column == 5) return new StationTile("Lhasa");
+                        if (column == 10) return new StationTile("Chengdu");
+                        if (column == 14) return new StationTile("Wuhan");
                         return TrainsTile.Empty;
                     }
                     break;
                 case 10:
-                    if (column >= 6 && column <= 12)
+                    if (column >= 8 && column <= 16) return TrainsTile.Empty;
+                    break;
+                case 11:
+                    if (column >= 8 && column <= 16)
                     {
-                        if (column == 11) return new StationTile("Guangzhou");
+                        if (column == 10) return new StationTile("Kunming");
+                        return TrainsTile.Empty;
+                    }
+                    break;
+                case 12:
+                    if ((column >= 8 && column <= 10) || (column >= 12 && column <= 14))
+                    {
+                        if (column == 14) return new StationTile("Guangzhou");
                         return TrainsTile.Empty;
                     }
                     break;
@@ -92,7 +102,7 @@ namespace Hextasy.Trains
             return null;
         }
 
-        public int Columns { get { return 16; } }
-        public int Rows { get { return 11; } }
+        public int Columns { get { return 20; } }
+        public int Rows { get { return 13; } }
     }
 }
---
+++ Hextasy.Trains/MapUnitedStates.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.Composition;
+
+namespace Hextasy.Trains
+{
+    [Export(typeof(IMap))]
+    public class MapUnitedStates : IMap
+    {
+        private readonly IEnumerable<string> _stations = new[]
+        {
+            "Boston", "Bismarck", "Helena", "Carson City", "Denver", "Washington",
+            "Indianapolis", "Los Angeles", "Atlanta", "Oklahoma City", "Miami", "Austin"
+        };
+
+        public string Name { get { return "United States of America"; } }
+        public IEnumerable<string> Stations { get { return _stations; } }
+
+        public TrainsTile CreateTile(int column, int row)
+        {
+            switch (row)
+            {
+                case 0:
+                    if ((column >= 1 && column <= 3) || column == 5) return TrainsTile.Empty;
+                    break;
+                case 1:
+                    if ((column >= 1 && column <= 10) || (column >= 17 && column <= 18))
+                    {
+                        if (column == 4) return new StationTile("Helena");
+                        if (column == 8) return new StationTile("Bismarck");
+                        if (column == 17) return new StationTile("Boston");
+                        return TrainsTile.Empty;
+                    }
+                    break;
+                case 2:
+                    if ((column >= 0 && column <= 11) || column == 13 || (column >= 15 && column <= 17))
+                        return TrainsTile.Empty;
+                    break;
+                case 3:
+                    if ((column >= 0 && column <= 11) || column == 13 || (column >= 15 && column <= 16))
+                    {
+                        if (column == 1) return new StationTile("Carson City");
+                        return TrainsTile.Empty;
+                    }
+                    break;
+                case 4:
+                    if (column >= 0 && column <= 16)
+                    {
+                        if (column == 6) return new StationTile("Denver");
+                        if (column == 12) return new StationTile("Indianapolis");
+                        if (column == 16) return new StationTile("Washington");
+                        return TrainsTile.Empty;
+                    }
+                    break;
+                case 5:
+                    if (column >= 0 && column <= 16)
+                    {
+                        if (column == 1) return new StationTile("Los Angeles");
+                        return TrainsTile.Empty;
+                    }
+                    break;
+                case 6:
+                    if (column >= 2 && column <= 15)
+                    {
+                        if (column == 8) return new StationTile("Oklahoma City");
+                        if (column == 13) return new StationTile("Atlanta");
+                        return TrainsTile.Empty;
+                    }
+                    break;
+                case 7:
+                    if (column == 4 ||(column >= 6 && column <= 14)) return TrainsTile.Empty;
+                    break;
+                case 8:
+                    if (column == 6 || (column >= 8 && column <= 10)|| (column >= 14 && column <= 15))
+                    {
+                        if (column == 8) return new StationTile("Austin");
+                        if (column == 15) return new StationTile("Miami");
+                        return TrainsTile.Empty;
+                    }
+                    break;
+                case 9:
+                    if (column == 8) return TrainsTile.Empty;
+                    break;
+            }
+
+            return null;
+        }
+
+        public int Columns { get { return 19; } }
+        public int Rows { get { return 10; } }
+    }
+}

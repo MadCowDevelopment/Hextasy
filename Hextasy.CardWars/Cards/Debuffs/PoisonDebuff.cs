ec0da424df1cf5edce0bff2bf4bdf1ccf9614aeb
Mad <devdumbblind@googlemail.com>
2014-02-16 15:22:49 +0800

    added poison trait and debuff

---
+++ Hextasy.CardWars/Cards/Debuffs/PoisonDebuff.cs
@@ -0,0 +1,15 @@
+﻿namespace Hextasy.CardWars.Cards.Debuffs
+{
+    public class PoisonDebuff : Debuff, IActivateDebuffOnStartTurn
+    {
+        public override string ImageFilename
+        {
+            get { return @"Images\Cards\Debuffs\fog-acid-3.png"; }
+        }
+
+        public override void Activate(MonsterCard affectedCard)
+        {
+            affectedCard.TakeDamage(1);
+        }
+    }
+}
---
+++ Hextasy.CardWars/Cards/Traits/PoisonWeaponTrait.cs
@@ -0,0 +1,17 @@
+﻿using Hextasy.CardWars.Cards.Debuffs;
+
+namespace Hextasy.CardWars.Cards.Traits
+{
+    public class PoisonWeaponTrait : Trait, IActivateTraitOnAttack
+    {
+        public override string ImageFilename
+        {
+            get { return @"Images\Cards\Spells\enchant-acid-3.png"; }
+        }
+
+        public override void Activate(CardWarsGameLogic cardWarsGameLogic, CardWarsTile targetTile)
+        {
+            targetTile.Card.AddDebuff(new PoisonDebuff());
+        }
+    }
+}

aaa0a35c1fe5ef66acc8e7a6bb3337b63635870d
Mad <devdumbblind@googlemail.com>
2014-02-20 20:45:04 +0800

    added card: Smith

---
+++ Hextasy.CardWars/Cards/Monsters/SmithCard.cs
@@ -0,0 +1,49 @@
+﻿using System.ComponentModel.Composition;
+using Hextasy.CardWars.Cards.Traits;
+
+namespace Hextasy.CardWars.Cards.Monsters
+{
+    [Export(typeof(Card))]
+    public class SmithCard : MonsterCard
+    {
+        public SmithCard()
+        {
+            Traits.Add(new IncreaseMonsterAttackTrait(2));
+        }
+
+        public override string Name
+        {
+            get { return "Smitty the Smith"; }
+        }
+
+        public override string Description
+        {
+            get { return "Gives a friendly monster +2 attack at the end of your turn."; }
+        }
+
+        public override int Cost
+        {
+            get { return 4; }
+        }
+
+        protected override string ImageFilename
+        {
+            get { return "Smithy01.png"; }
+        }
+
+        public override int BaseAttack
+        {
+            get { return 1; }
+        }
+
+        public override int BaseHealth
+        {
+            get { return 4; }
+        }
+
+        public override Race Race
+        {
+            get { return Race.Human; }
+        }
+    }
+}
---
+++ Hextasy.CardWars/Cards/Traits/IncreaseMonsterAttackTrait.cs
@@ -0,0 +1,30 @@
+﻿using Hextasy.Framework;
+
+namespace Hextasy.CardWars.Cards.Traits
+{
+    public class IncreaseMonsterAttackTrait : Trait
+    {
+        private int Amount { get; set; }
+
+        public IncreaseMonsterAttackTrait(int amount)
+        {
+            Amount = amount;
+        }
+
+        public override string Name
+        {
+            get { return "Increase Monster Attack"; }
+        }
+
+        protected override string ImageFilename
+        {
+            get { return "Cards/Traits/Sword20.PNG"; }
+        }
+
+        public override void Activate(CardWarsGameLogic cardWarsGameLogic, CardWarsTile targetTile)
+        {
+            var friendlyMonster = cardWarsGameLogic.CurrentPlayerCards.RandomOrDefault();
+            if (friendlyMonster != null) friendlyMonster.AttackBonus += Amount;
+        }
+    }
+}

ad889cad88bb2c50a8e9fbc583a047984239a53d
Mihi <devdumbblind@googlemail.com>
2014-02-21 14:40:22 +0800

    added beast deck factory

---
+++ Hextasy.CardWars/DeckBuilders/BeastDeckFactory.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using Hextasy.CardWars.Cards;
+using Hextasy.CardWars.Cards.Monsters;
+
+namespace Hextasy.CardWars.DeckBuilders
+{
+    public class BeastDeckFactory : DeckFactory
+    {
+        protected override string Name
+        {
+            get { return "Beast"; }
+        }
+
+        protected override List<Card> Cards
+        {
+            get
+            {
+                return new List<Card>
+                {
+                    new BasiliskCard(),
+                    new BatCard(),
+                    new BrownBearCard(),
+                    new FireAntCard(),
+                    new HorseCard(),
+                    new SpiderCard(),
+                    new TurtleCard(),
+                    new WarElephantCard(),
+                    new WolfCard(),
+                    new WormCard(),
+                    new BasiliskCard(),
+                    new BatCard(),
+                    new BrownBearCard(),
+                    new FireAntCard(),
+                    new HorseCard(),
+                    new SpiderCard(),
+                    new TurtleCard(),
+                    new WarElephantCard(),
+                    new WolfCard(),
+                    new WormCard()
+                };
+            }
+        }
+    }
+}
